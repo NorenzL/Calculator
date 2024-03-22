926fece8c3962df8cfdc54399b8bfbc65763597f
Norenz Laurito <laurito.marknorenz@gmail.com>
2024-03-22 08:36:30 +0800

    Add calculate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+let operator;
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +16,21 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function calculate(operation, num1, num2) {
+  switch (operation) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
 }
