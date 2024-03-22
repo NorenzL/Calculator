2960ba8d7ae89860c89334b7c01d7e647d8bcbb5
Norenz Laurito <laurito.marknorenz@gmail.com>
2024-03-22 09:48:22 +0800

    Add button event listener

--- script.js
+++ script.js
@@ -34,3 +34,66 @@ function operate(operation, num1, num2) {
       break;
   }
 }
+
+function populateDisplay(input) {
+  let display = document.querySelector(".display");
+  if (input === "") {
+    display.textContent = "";
+  } else {
+    display.textContent += input;
+  }
+}
+
+// Add button click event listener
+const button = document.querySelectorAll(".button");
+
+button.forEach((buttonItem) => {
+  buttonItem.addEventListener("click", () => {
+    switch (buttonItem.textContent) {
+      case "1":
+        populateDisplay("1");
+        break;
+      case "2":
+        populateDisplay("2");
+        break;
+      case "3":
+        populateDisplay("3");
+        break;
+      case "4":
+        populateDisplay("4");
+        break;
+      case "5":
+        populateDisplay("5");
+        break;
+      case "6":
+        populateDisplay("6");
+        break;
+      case "7":
+        populateDisplay("7");
+        break;
+      case "8":
+        populateDisplay("8");
+        break;
+      case "9":
+        populateDisplay("9");
+        break;
+      case "0":
+        populateDisplay("0");
+        break;
+      case "+":
+        populateDisplay("+");
+        break;
+      case "-":
+        populateDisplay("-");
+        break;
+      case "*":
+        populateDisplay("*");
+        break;
+      case "/":
+        populateDisplay("/");
+        break;
+      case "clear":
+        populateDisplay("");
+    }
+  });
+});
